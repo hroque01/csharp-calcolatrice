bb3605e50b098f48ede109ddd40ec3887a64c9e9
Hadson Roque <nineonezero.light@gmail.com>
2023-04-12 16:02:15 +0200

    Create Snacks

--- csharp-calcolatrice/CalcoliHelper.cs
+++ csharp-calcolatrice/CalcoliHelper.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Channels;
 using System.Threading.Tasks;
 
 namespace csharp_calcolatrice
@@ -26,9 +27,65 @@ namespace csharp_calcolatrice
         {
             return x - y;
         }
+
+        //Differenza tra due numeri double
         public static double Differenza(double x, double y)
         {
             return x - y;
         }
+
+        //Moltiplicazione di due numeri interi
+        public static int Moltiplicazione( int x, int y)
+        {
+            return x * y;
+        }
+        public static double Moltiplicazione(double x, double y)
+        {
+            return x * y;
+        }
+
+        // Valore assoluto di un numero intero
+        public static int ValoreAssoluto(int x)
+        {
+            return Math.Abs(x);
+        }
+
+        //Valore assoluto di un numero double
+        public static double ValoreAssoluto(double x)
+        {
+            return Math.Abs(x);
+        }
+
+        //Minimo tra due numeri interi
+        public static int Minimo (int x, int y)
+        {
+            if (x < y)
+            {
+                return x;
+
+            } 
+            else
+            {
+
+                return y;
+
+            }
+        }
+        //Minimo tra due numeri double
+        public static double Minimo(double x, double y)
+        {
+            if (x < y)
+            {
+                return x;
+
+            }
+            else
+            {
+
+                return y;
+
+            }
+        }
+
     }
 }
