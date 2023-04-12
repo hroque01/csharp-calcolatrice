c6e0093f3bae3e28ac4bac22c302ce4b98bf2c53
Hadson Roque <nineonezero.light@gmail.com>
2023-04-12 15:53:59 +0200

    Add project files.

---
+++ csharp-calcolatrice/CalcoliHelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_calcolatrice
+{
+    internal class CalcoliHelper
+    {
+        // Somma di due numeri interi
+        public static int Somma(int x, int y)
+        {
+            return x + y;
+        }
+
+
+        // Somma di due numeri double
+        public static double Somma(double x, double y)
+        {
+            return x + y;
+        }
+
+        //Differenza tra due numeri interi
+        public static int Differenza(int x, int y)
+        {
+            return x - y;
+        }
+        public static double Differenza(double x, double y)
+        {
+            return x - y;
+        }
+    }
+}
---
+++ csharp-calcolatrice/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace csharp_calcolatrice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
